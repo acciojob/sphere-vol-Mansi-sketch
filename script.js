ec44f19e0f67fe13765ea2c562d51d9cf3614bf5
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-07-25 21:12:29 +0200

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function volume_sphere() {
 	let radiusVal = parseFloat(document.getElementById('radius').value);
 	let volumeVal = 0;
-	if (typeof radiusVal === 'string' || radiusVal < 0) {
+	if ((typeof radiusVal === 'string' || radiusVal < 0) && typeof radiusVal !== '') {
 		return NaN;
 	}
-	
 	volumeVal = Math.PI * (4/3) * Math.pow(radiusVal,3);
 	document.getElementById('volume').value = volumeVal.toFixed(4);
 } 
